77f4c22769a7c99bb19a1338b135404efa45c3c9
nissy220 <west_sophia2@kcf.biglobe.ne.jp>
2020-12-11 22:50:20 +0900

    Add files via upload

--- calc_stopping_time.py
+++ calc_stopping_time.py
@@ -6,7 +6,7 @@ Created on Sat Dec  5 21:39:37 2020
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Definition of parameters
+#Definitions of parameters
 sigma=0.5*np.log(3)
 v=0.5*np.log(0.75)/sigma
 s=0.5*np.log(3)/sigma
@@ -34,31 +34,31 @@ def calc_stopping_time(ns, ne, t_range):
             t_max=count
     return hist, t_max     
 
-#Approximation of the erfcx function
+#Approximation of the scaled complementary error function
 def erfcx(x):
     a=1.98
     b=1.135
     return (1-np.exp(-a*x))/(b * np.sqrt(np.pi)*x)
 
-#Function that approximates the stopping time distribution by the Brownian motion model           
+#Function that approximates the toatl stopping time distribution by the Brownian motion          
 def phi(n, T):
-    C=np.sqrt(2)*sigma*s*(-s/np.sqrt(beta)-0.5*(3*s-v)/np.sqrt(alpha))
-    D=-np.sqrt(2)*sigma*s*(s/np.sqrt(beta)-0.5*(3*s-v)/np.sqrt(alpha))
+    C=-s/np.sqrt(beta)-0.5*(3*s-v)/np.sqrt(alpha)
+    D=-s/np.sqrt(beta)+0.5*(3*s-v)/np.sqrt(alpha)
     
     t=(np.log(n)/sigma+2*s*T)/(3*s-v)
-    x=np.sqrt(alpha*t)+T*np.sqrt(beta/t)
-    y=-np.sqrt(alpha*t)+T*np.sqrt(beta/t)
+    X=np.sqrt(alpha*t)+T*np.sqrt(beta/t)
+    Y=-np.sqrt(alpha*t)+T*np.sqrt(beta/t)
     z=-alpha*t-beta*T**2/t
     g=(6*s**2-2*sigma*s)*T+z
       
-    return (C*erfcx(x)+D*erfcx(y))*np.exp(g)   
+    return np.sqrt(2)*sigma*s*(C*erfcx(X)+D*erfcx(Y))*np.exp(g)   
 
-#Estimate the stopping time distribution for the numbers from ns to ne
+#Estimate the toatl stopping time distribution for the numbers from ns to ne
 def estimate_distribution(ns, ne, t_range):
     T=np.arange(1, t_range)
     return phi(ne, T) - phi(ns, T)
 
-#Estimate the max of stopping time for the numbers from 1 to n
+#Estimate the max of toatl stopping time for the numbers from 1 to n
 def estimate_max_stopping_time(n, t_range): 
     est_hist=estimate_distribution(1, n, t_range ) 
     return np.argmin(np.abs(est_hist[30:]-1))    
@@ -71,38 +71,37 @@ def estimate_max_stopping_time(n, t_range):
 #    
 #
 
-#range of the stopping time
+#range of the toatl stopping time
 t_range=500
 
 #range of numbers
 ns=1
 ne=10**5
 
-#Calculate and estimate the stopping time distribution 
+#Calculate and estimate the toatl stopping time distribution 
 est_hist=estimate_distribution(ns, ne, t_range) 
 
 hist, t_max=calc_stopping_time(ns, ne, t_range)
-ave_range=5
+ave_range=3
 #Smooth the histgram
 filter=np.ones(ave_range) / ave_range
 hist=np.convolve(hist,filter, mode='same')
 
-plt.plot(np.arange(1, t_range), hist, label='Collatz sequence ')
+plt.plot(np.arange(1, t_range), hist, label='Collatz sequences')
 plt.plot(np.arange(1, t_range),est_hist, label='Brownian motion model')
-plt.xlabel('stopping time')
+plt.xlabel('total stopping time')
 plt.ylabel('counts')
-plt.title('Stopping time distribution from' + "{:10.1e}".format(ns) + ' to' + "{:10.1e}".format(ne))
+plt.title('Total stopping time from' + "{:10.1e}".format(ns) + ' to' + "{:10.1e}".format(ne))
 plt.legend()
 plt.show()
 
-t_range=3000
-
-#Estimate the max of the stopping time
+t_range2=3000
+#
+##Estimate the max of the total stopping time
 for i in range(4, 18):
     n=10**i
-    pred_t_max=estimate_max_stopping_time(n, t_range)
-#    print('max of stopping time =', t_max)
-    print('less than'+ "{:10.1e}".format(n) + ', prediction of max of stopping time =', pred_t_max, 'steps')
+    pred_t_max=estimate_max_stopping_time(n, t_range2)
+    print('less than'+ "{:10.1e}".format(n) + ', prediction of max of total stopping time =', pred_t_max, 'steps')
 
 
     
