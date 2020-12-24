9b663eaa56a14016570233241a6c827d5e07ab0f
nissy220 <west_sophia2@kcf.biglobe.ne.jp>
2020-12-24 22:35:58 +0900

    Add files via upload

--- src/calc_stopping_time.py
+++ src/calc_stopping_time.py
@@ -6,7 +6,7 @@ Created on Sat Dec  5 21:39:37 2020
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Definitions of parameters
+#Definitions of parameters.
 sigma=0.5*np.log(3)
 v=0.5*np.log(0.75)/sigma
 s=0.5*np.log(3)/sigma
@@ -34,13 +34,13 @@ def calc_stopping_time(ns, ne, t_range):
             t_max=count
     return hist, t_max     
 
-#Approximation of the scaled complementary error function
+#Approximation of the scaled complementary error function.
 def erfcx(x):
     a=1.98
     b=1.135
     return (1-np.exp(-a*x))/(b * np.sqrt(np.pi)*x)
 
-#Function that approximates the total stopping time distribution by the Brownian motion          
+#Function that estimates the total stopping time distribution of the Collatz problem.          
 def log_phi(r, p, T):    
     x=np.log(r)+p*np.log(10)
     C=-s/np.sqrt(beta)-0.5*(3*s-v)/np.sqrt(alpha)
@@ -54,13 +54,13 @@ def log_phi(r, p, T):
       
     return g + np.log(np.sqrt(2)*sigma*s) + np.log(C*erfcx(X)+D*erfcx(Y))   
 
-#Estimate the total stopping time distribution for the numbers from ns=rs*10**ps to ne=re*10**pe
+#Estimate the total stopping time distribution for numbers in [ns=rs*10**ps, ne=re*10**pe].
 def estimate_distribution(rs, ps, re, pe, t_range):
     T=np.arange(t_range)
     ps = max(ps, 1)
     return np.exp(log_phi(re, pe, T)) - np.exp(log_phi(rs, ps, T))
 
-#Estimate the max of total stopping time for the numbers from 1 to n=r*10**p
+#Estimate the max of total stopping time for the numbers in [1, n=r*10**p].
 def estimate_max_stopping_time(r, p, t_range): 
     T=np.arange(t_range)
     log_count=log_phi(r, p, T)
@@ -69,6 +69,7 @@ def estimate_max_stopping_time(r, p, t_range):
     return min_idx    
 
 #Inverse Gaussian distribution 
+#This function is equal to the derivative of phi.
 def estimate_density(r, p, t_range):
     T=np.arange(t_range)
     x=(np.log(r)+p*np.log(10))/sigma
@@ -76,15 +77,15 @@ def estimate_density(r, p, t_range):
     t=(x+2*s*T)/(3*s-v)
     return 2*s/(3*s-v)/np.sqrt(2*np.pi*t**3)*x*np.exp(-0.5*(x+v*t)**2/t)
     
-def estimate_max_stopping_time2(r, p, t_range): 
+def estimate_max_stopping_time_from_density(r, p, t_range): 
     log_count=np.log(estimate_density(r, p, t_range)) + np.log(r) + p * np.log(10)
     max_idx=np.argmax(log_count)
     min_idx=np.argmin(np.abs(log_count[max_idx:])) + max_idx - 1
     return min_idx  
 
-#range of the total stopping time
+#Range of the total stopping time
 t_range=500
-#range of numbers [rs*10^ps, re*10^pe]
+#Range of numbers [rs*10^ps, re*10^pe]
 rs=1
 re=1
 ps=0
@@ -102,39 +103,40 @@ hist=np.convolve(hist,filter, mode='same')
 
 plt.plot(np.arange(t_range), hist, label='Collatz sequences')
 plt.plot(np.arange(t_range),est_hist, label='Brownian motion model')
-#plt.plot(np.arange(t_range),est_hist2, label='inverse')
 
-plt.xlabel('total stopping time')
-plt.ylabel('counts')
+plt.xlabel('Total stopping time')
+plt.ylabel('Counts')
 plt.title('Total stopping time from' + "{:10.1e}".format(rs*10**ps) + ' to' + "{:10.1e}".format(re*10**pe))
 plt.legend()
 plt.show()
 
 #Estimate the max of the total stopping time
-t_range2=2500
+t_range2=7500
 
 #List of max of the total stopping time
-collatz_t_max = np.array([0, 19, 118, 178, 261, 350, 524, 685, 949, 986, 1132, 1228,
+collatz_t_max = np.array([19, 118, 178, 261, 350, 524, 685, 949, 986, 1132, 1228,
                  1348, 1563, 1662, 1862, 1958, 2091, 2283])
 
 ##Estimate the max of the total stopping time
-power_range=18
-pred_t_max=np.zeros(power_range + 1)
-for i in range(1, power_range + 1):
-    pred_t_max[i]=estimate_max_stopping_time(1, i, t_range2)
+power_range=50
+pred_t_max=np.zeros(power_range)
+for i in range(power_range):
+    pred_t_max[i]=estimate_max_stopping_time(1, i+1, t_range2)
 
-plt.plot(np.arange(len(collatz_t_max)), collatz_t_max, 'o', label='Collatz sequences') 
-plt.plot(np.arange(power_range + 1), pred_t_max, 'o', label='Brownian motion model')  
+plt.plot(np.arange(1, len(collatz_t_max) + 1), collatz_t_max, 'o', label='Collatz sequences') 
+plt.plot(np.arange(1, power_range + 1), pred_t_max, 'o', label='Brownian motion model')  
  
-plt.xlabel('log_10(n)')   
+plt.xlabel('$log_{10}n$')   
 plt.ylabel('Max of total stopping time')
 plt.legend()
 plt.show()
 
 error=np.zeros(len(collatz_t_max))
-error[1:]=np.abs(pred_t_max[1:len(collatz_t_max)]-collatz_t_max[1:]) / collatz_t_max[1:]
-plt.plot(np.arange(2,len(collatz_t_max)), error[2:], 'o')             
-plt.xlabel('log_10(n)')   
-plt.ylabel('Error')            
+error=(pred_t_max[:len(collatz_t_max)]-collatz_t_max) / collatz_t_max
+plt.plot(np.arange(3,len(collatz_t_max)+1), np.abs(error[2:]), 'o')             
+plt.xlabel('$log_{10}n$')   
+plt.ylabel('Estimation error')    
+plt.show()
+        
     
          
